5468d64a05f6376f17deba3c5b0f692c13c8f3d0
mku777 <mkm.kuz777@gmail.com>
2022-10-05 15:13:18 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,54 @@
-
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryElements = document.querySelector(".gallery");
 
-const createImageGallery = galleryItems.map(
-    ({ original, preview, description }) =>
-      `<div class="gallery__item">
-<a class="gallery__link" href="${original}">
-  <img
-    class="gallery__image"
-    src="${preview}"
-    data-source="large-image.jpg"
-    alt="${description}"
-  />
-</a>
-</div>`
-  )
-  .join("");
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+            />
+        </a>
+    </div>
+    `
+    )
+    .join("");
+}
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+galleryElements.insertAdjacentHTML("beforeend", addGalleryMarkup);
 
-galleryElements.insertAdjacentHTML("beforeend", createImageGallery);
+//  basicLightbox library
 
+galleryElements.addEventListener("click", onLargeImageShow);
 
+function onLargeImageShow(event) {
+  event.preventDefault();
+  if (event.target.classList.value !== "gallery__image") {
+    return;
+  }
 
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">
+    `
+  );
+  instance.show();
 
+  if (instance.visible()) {
+    window.addEventListener("keydown", onPressEsc);
+  }
 
-console.log(galleryItems);
+  function onPressEsc(event) {
+    if (event.code === "Escape") {
+      modal.close();
+      window.removeEventListener("keydown", onPressEsc);
+    }
+  }
+}

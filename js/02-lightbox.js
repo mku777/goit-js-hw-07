529759d24131577e7c5aadef7c10bd1741cbc9e2
mku777 <mkm.kuz777@gmail.com>
2022-10-05 17:12:50 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElements = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `
+          <a class="gallery__item" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+      `
+    )
+    .join("");
+}
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+galleryElements.insertAdjacentHTML("beforeend", addGalleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});

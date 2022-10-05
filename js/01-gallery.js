d77ce576080f186500a1ee5dc651ae464d3253db
mku777 <mkm.kuz777@gmail.com>
2022-10-05 11:13:54 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const galleryElements = document.querySelector(".gallery");
+
+const createImageGallery = galleryItems.map(
+    ({ original, preview, description }) =>
+      `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="large-image.jpg"
+    alt="${description}"
+  />
+</a>
+</div>`
+  )
+  .join("");
+
+galleryElements.insertAdjacentHTML("beforeend", createImageGallery);
+
+
+
+
 
 console.log(galleryItems);
